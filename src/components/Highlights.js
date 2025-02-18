433b7efb98c254e59092535feb4eb5d677860a49
Peter Schmid <peter.schmid@codeunity.de>
2025-02-18 17:10:17 +0100

    di

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -2,29 +2,32 @@ import React from 'react';
 import "./Highlights.css";
 import iconSunrise from "../images/icons/sun/sunrise-svgrepo-com.svg";
 import iconSunset from "../images/icons/sun/sunset-svgrepo-com.svg";
+import UVIndex from './UVIndex.js'; // Importiere die UVIndex-Komponente
 
 export default function Highlights({ highlights }) {
     return (
-    <div className="highlights-container">
-        <h2>Today's Highlights</h2>
-        <div className="highlights">
-            {highlights && highlights.map((item, index) => (
-            <div key={index} className="highlight-box">
-                <h3>{item.title}</h3>
-                {item.title === "Sunrise & Sunset" ? (
-                <div className="sunrise-sunset">
-                    <p><img src={iconSunrise} alt="Sunrise" />{item.up}</p>
-                    <p><img src={iconSunset} alt="Sunset" />{item.down}</p>
+        <div className="highlights-container">
+            <h2>Today's Highlights</h2>
+            <div className="highlights">
+                {highlights && highlights.map((item, index) => (
+                    <div key={index} className="highlight-box">
+                        <h3>{item.title}</h3>
+                        {item.title === "Sunrise & Sunset" ? (
+                            <div className="sunrise-sunset">
+                                <p><img src={iconSunrise} alt="Sunrise" />{item.up}</p>
+                                <p><img src={iconSunset} alt="Sunset" />{item.down}</p>
+                            </div>
+                        ) : item.title === "UV Index" ? (
+                            <UVIndex value={item.value} />
+                        ) : (
+                            <div>
+                                <p><strong>{item.value}</strong> {item.unit}</p>
+                                <p>{item.status}</p>
+                            </div>
+                        )}
                     </div>
-                ) : (
-                <div>
-                    <p><strong>{item.value}</strong> {item.unit}</p>
-                    <p>  {item.status}</p>
-                </div>
-                )}  
+                ))}
             </div>
-            ))}
         </div>
-    </div>
     );
 }
--- src/components/UVIndex.js
+++ src/components/UVIndex.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './UVIndex.css';
 import { getStatusUV } from '../utils/statusUtils';
 
-const UVIndex = ({ value }) => {
+const UVIndex = ({ value, status }) => {
     const uvLevel = getStatusUV(value).toLowerCase().split(' ')[0]; // Extrahiere den UV-Level
-
+    console.log(uvLevel);
+    
     return (
         <div className={`uv-index ${uvLevel}`}>
             <div className="uv-bar" style={{ width: `${(value / 11) * 100}%` }}></div>
             <div className="uv-value">{value}</div>
+            <div className="uv-status">{getStatusUV(value)}</div>
         </div>
     );
 };
