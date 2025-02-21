cdfd840bc950b8ab92508d99c22f545abb5e080b
Peter Schmid <peter.schmid@codeunity.de>
2025-02-21 10:43:40 +0100

    gradient

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import "./App.css";
 
 import Forecast from "./components/Forecast";
-import Highlights from "./components/Highlights";   
+import Highlights from "./components/Highlights";
 import Sidebar from "components/Sidebar";
 
 import { getWeatherImage, getWeatherIcons } from "./utils/weatherUtils";
-import { getStatusUV, getStatusWind, getStatusVisibility, getStatusHumidity, getStatusAirquality } from "./utils/statusUtils";
+import {
+  getStatusUV,
+  getStatusWind,
+  getStatusVisibility,
+  getStatusHumidity,
+  getStatusAirquality,
+} from "./utils/statusUtils";
 
 import getCloudy from "./images/cloudy.svg";
 import getRain from "./images/rain.svg";
@@ -15,197 +21,238 @@ import getThunderstorm from "./images/thunderstorm.svg";
 import getClear from "./images/clear.svg";
 import getFog from "./images/fog.svg";
 
-
 export default function WeatherApp() {
-    const [inputCity, setInputCity] = useState("Lossburg");
-    const [city, setCity] = useState("Lossburg");
-    const [weather, setWeather] = useState(null);
-    const [country, setCountry] = useState("");
-    const [highlights, setHighlights] = useState([]);
-    const [forecastData, setForecastData] = useState([]);
-    const [error, setError] = useState("");
-    const [timezoneOffset, setTimezoneOffset] = useState(0);
-    const [isCelsius, setIsCelsius] = useState(true);
+  const [inputCity, setInputCity] = useState("Lossburg");
+  const [city, setCity] = useState("Lossburg");
+  const [weather, setWeather] = useState(null);
+  const [country, setCountry] = useState("");
+  const [highlights, setHighlights] = useState([]);
+  const [forecastData, setForecastData] = useState([]);
+  const [error, setError] = useState("");
+  const [timezoneOffset, setTimezoneOffset] = useState(0);
+  const [isCelsius, setIsCelsius] = useState(true);
 
-    const timezoneOffsetFormatted = timezoneOffset >= 0 ? `+${timezoneOffset}` : timezoneOffset;
+  const timezoneOffsetFormatted =
+    timezoneOffset >= 0 ? `+${timezoneOffset}` : timezoneOffset;
 
-    const convertTemperature = (temp) => {
-        return isCelsius ? temp : (temp * 9/5 + 32).toFixed(1);
-    };
+  const getCurrentTime = () => {
+    const now = new Date();
+    return now.toLocaleTimeString("de-DE", {
+      second: undefined,
+      hour: "2-digit",
+      minute: "2-digit",
+      hourCycle: "h23",
+    });
+  };
 
-    const getCurrentTime = () => {
-        const now = new Date();
-        return now.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' });
-    };
+  const getCurrentDay = () => {
+    const now = new Date();
+    const options = { weekday: "long" };
+    return now.toLocaleDateString("en-En", options);
+  };
 
-    const getCurrentDay = () => {
-        const now = new Date();
-        const options = { weekday: 'long' };
-        return now.toLocaleDateString('de-DE', options);
-    };
+  const getNextSevenDays = () => {
+    const days = [];
+    const options = { weekday: "short" };
+    for (let i = 0; i < 7; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() + i);
+      days.push(date.toLocaleDateString("en-EN", options));
+    }
+    return days;
+  };
 
-    const getNextSevenDays = () => {
-        const days = [];
-        const options = { weekday: 'short' };
-        for (let i = 0; i < 7; i++) {
-            const date = new Date();
-            date.setDate(date.getDate() + i);
-            days.push(date.toLocaleDateString('de-DE', options));
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+  const [currentDay] = useState(getCurrentDay());
+  const [forecastDays] = useState(getNextSevenDays());
+
+  const apiKey = "15602a28018e8d952821183312c1099c";
+
+  const getWeatherData = useCallback(
+    async (city) => {
+      try {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+          throw new Error("Could not fetch weather data");
         }
-        return days;
-    };
 
-    const [currentTime, setCurrentTime] = useState(getCurrentTime());
-    const [currentDay, setCurrentDay] = useState(getCurrentDay());
-    const [forecastDays, setForecastDays] = useState(getNextSevenDays());
+        const data = await response.json();
+        
+        setCountry(data.sys.country);
+        setTimezoneOffset(data.timezone / 3600);
 
-    const apiKey = "15602a28018e8d952821183312c1099c"; 
+        setWeather({
+          city: data.name,
+          temp: (data.main.temp - 273.15).toFixed(1),
+          humidity: data.main.humidity,
+          visibility: data.visibility,
+          description: data.weather[0].description,
+          image: getWeatherImage(data.weather[0].id),
+          icon: getWeatherIcons(data.weather[0].id),
+        });
 
-    const getWeatherData = useCallback(async (city) => {
-        try {
-            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-            const response = await fetch(apiUrl);
+        setCity(data.name); // Setze den Stadtwert auf den tatsächlich gefundenen Wert
 
-            if (!response.ok) {
-                throw new Error("Could not fetch weather data");
-            }
+        const uvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
+        const uvResponse = await fetch(uvUrl);
+        const uvData = await uvResponse.json();
+        console.log(uvData); // Ausgabe der UV-Daten in der Konsole
 
-            const data = await response.json();
-            console.log(data); // Ausgabe der API-Antwort in der Konsole
+        const sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString(
+          "de-DE",
+          { hour: "2-digit", minute: "2-digit", hourCycle: "h23" }
+        );
+        const sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString(
+          "de-DE",
+          { hour: "2-digit", minute: "2-digit", hourCycle: "h23" }
+        );
 
-            setCountry(data.sys.country);
-            setTimezoneOffset(data.timezone / 3600);
+        const airQualityUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
+        const airQualityResponse = await fetch(airQualityUrl);
+        const airQualityData = await airQualityResponse.json();
+        // console.log(airQualityData); // Ausgabe der Luftqualitätsdaten in der Konsole
 
-            setWeather({
-                city: data.name,
-                temp: (data.main.temp - 273.15).toFixed(1),
-                humidity: data.main.humidity,
-                visibility: data.visibility,
-                description: data.weather[0].description,
-                image: getWeatherImage(data.weather[0].id),
-                icon: getWeatherIcons(data.weather[0].id),
-            });
+        // const forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+        // const forecastResponse = await fetch(forecastUrl);
+        // const forecastData = await forecastResponse.json();
+        // console.log(forecastData); // Ausgabe der Vorhersagedaten in der Konsole
 
-            setCity(data.name); // Setze den Stadtwert auf den tatsächlich gefundenen Wert
+        // setForecastData(forecastData.daily.slice(1, 8).map((day, index) => ({
+        //     day: forecastDays[index],
+        //     image: getWeatherImage(day.weather[0].id),
+        //     description: day.weather[0].description,
+        //     tempMin: (day.temp.min - 273.15).toFixed(1),
+        //     tempMax: (day.temp.max - 273.15).toFixed(1)
+        // })));
 
-            const uvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
-            const uvResponse = await fetch(uvUrl);
-            const uvData = await uvResponse.json();
-            console.log(uvData); // Ausgabe der UV-Daten in der Konsole
+        setForecastData([
+          {
+            day: forecastDays[1],
+            image: getCloudy,
+            minTemp: "12",
+            maxTemp: "22",
+          },
+          { day: forecastDays[2], image: getRain, minTemp: "", maxTemp: "" },
+          { day: forecastDays[3], image: getSnow, minTemp: "", maxTemp: "" },
+          {
+            day: forecastDays[4],
+            image: getThunderstorm,
+            minTemp: "",
+            maxTemp: "",
+          },
+          { day: forecastDays[5], image: getClear, minTemp: "", maxTemp: "" },
+          { day: forecastDays[6], image: getFog, minTemp: "", maxTemp: "" },
+        ]);
 
-            const sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' });
-            const sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' });
+        setHighlights([
+          {
+            title: "UV Index",
+            value: uvData.value,
+            unit: "",
+            status: getStatusUV(uvData.value),
+          },
+          {
+            title: "Wind Status",
+            value: data.wind.speed,
+            unit: "km/h",
+            status: getStatusWind(data.wind.speed),
+          },
+          { title: "Sunrise & Sunset", up: `${sunrise}`, down: `${sunset}` },
+          {
+            title: "Humidity",
+            value: data.main.humidity,
+            unit: "%",
+            status: getStatusHumidity(data.main.humidity),
+          },
+          {
+            title: "Visibility",
+            value: (data.visibility / 1000).toFixed(1),
+            unit: "",
+            status: getStatusVisibility((data.visibility / 1000).toFixed(1)),
+          },
+          {
+            title: "Air Quality",
+            value: airQualityData.list[0].main.aqi,
+            unit: "",
+            status: getStatusAirquality(airQualityData.list[0].main.aqi),
+          }, // Air quality index
+        ]);
+        setError("");
+      } catch (error) {
+        setWeather(null);
+        setHighlights([]);
+        setError("City not found or API issue!");
+      }
+    },
+    [apiKey, forecastDays]
+  );
 
-            const airQualityUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
-            const airQualityResponse = await fetch(airQualityUrl);
-            const airQualityData = await airQualityResponse.json();
-            // console.log(airQualityData); // Ausgabe der Luftqualitätsdaten in der Konsole
+  useEffect(() => {
+    getWeatherData("Lossburg"); // Abruf der Wetterdaten für Lossburg beim ersten Laden
+  }, [getWeatherData]);
 
-            const forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
-            const forecastResponse = await fetch(forecastUrl);
-            const forecastData = await forecastResponse.json();
-            console.log(forecastData); // Ausgabe der Vorhersagedaten in der Konsole
+  useMemo(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 60000); // Aktualisiert die Zeit jede Minute
+    return () => clearInterval(interval); // Bereinigt das Intervall beim Demontieren der Komponente
+  }, []);
 
-            // setForecastData(forecastData.daily.slice(1, 8).map((day, index) => ({
-            //     day: forecastDays[index],
-            //     image: getWeatherImage(day.weather[0].id),
-            //     description: day.weather[0].description,
-            //     tempMin: (day.temp.min - 273.15).toFixed(1),
-            //     tempMax: (day.temp.max - 273.15).toFixed(1)
-            // })));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (inputCity.trim()) {
+      getWeatherData(inputCity);
+    } else {
+      setError("Please enter a city!");
+    }
+  };
 
-            setForecastData([
-                { day: forecastDays[1], image: getCloudy, minTemp: "12", maxTemp: "22"},
-                { day: forecastDays[2], image: getRain,  minTemp: "", maxTemp: ""},
-                { day: forecastDays[3], image: getSnow,  minTemp: "", maxTemp: "" },
-                { day: forecastDays[4], image: getThunderstorm, minTemp: "", maxTemp: "" },
-                { day: forecastDays[5], image: getClear,  minTemp: "", maxTemp: "" },
-                { day: forecastDays[6], image: getFog,  minTemp: "", maxTemp: "" },
-            ]);
+  const getCountryFlagEmoji = (countryCode) => {
+    return countryCode
+      .toUpperCase()
+      .replace(/./g, (char) =>
+        String.fromCodePoint(127397 + char.charCodeAt())
+      );
+  };
 
-            setHighlights([
-                { title: "UV Index", value: uvData.value, unit: "", status: getStatusUV(uvData.value) },
-                { title: "Wind Status", value: data.wind.speed, unit: "km/h", status: getStatusWind(data.wind.speed) },
-                { title: "Sunrise & Sunset", up: `${sunrise}`, down:`${sunset}`},
-                { title: "Humidity", value: data.main.humidity, unit: "%", status : getStatusHumidity(data.main.humidity) },
-                { title: "Visibility", value: (data.visibility / 1000).toFixed(1), unit: "", status: getStatusVisibility((data.visibility / 1000).toFixed(1)) },
-                { title: "Air Quality", value: airQualityData.list[0].main.aqi, unit: "", status: getStatusAirquality(airQualityData.list[0].main.aqi) }, // Air quality index
-            ]);
-            setError("");
-        } catch (error) {
-            setWeather(null);
-            setHighlights([]);
-            setError("City not found or API issue!");
-        }
-    }, [apiKey, forecastDays]);
+  return (
+    <div className="weather-grid">
+      <Sidebar
+        city={inputCity}
+        setCity={setInputCity}
+        handleSubmit={handleSubmit}
+        weather={weather}
+        currentDay={currentDay}
+        currentTime={currentTime}
+        isCelsius={isCelsius}
+      />
 
-    useEffect(() => {
-        getWeatherData("Lossburg"); // Abruf der Wetterdaten für Lossburg beim ersten Laden
-    }, [getWeatherData]);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentTime(getCurrentTime());
-        }, 60000); // Aktualisiert die Zeit jede Minute
-        return () => clearInterval(interval); // Bereinigt das Intervall beim Demontieren der Komponente
-    }, []);
-
-    useEffect(() => {
-        setCurrentDay(getCurrentDay());
-    }, []);
-
-    useEffect(() => {
-        setForecastDays(getNextSevenDays());
-    }, []);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (inputCity.trim()) {
-            getWeatherData(inputCity);
-        } else {
-            setError("Please enter a city!");
-        }
-    };
-
-    const getCountryFlagEmoji = (countryCode) => {
-        return countryCode
-            .toUpperCase()
-            .replace(/./g, char => String.fromCodePoint(127397 + char.charCodeAt()));
-    };
-
-    const cityExists = weather && !error;
-
-    return (
-        <div className="weather-grid">
-            <Sidebar
-                city={inputCity} 
-                setCity={setInputCity} 
-                handleSubmit={handleSubmit} 
-                weather={weather} 
-                currentDay={currentDay} 
-                currentTime={currentTime} 
-                isCelsius={isCelsius} 
-            />   
-            
-            {cityExists &&  weather && (
-                <div className="header">{city}, {getCountryFlagEmoji(country)} UTC{timezoneOffsetFormatted}</div>
-            )}
-            {error && <p className="errorDisplay">{error}</p>}
-
-            {weather && (
-                <div className="toggle-buttons">
-                    <button onClick={() => setIsCelsius(true)} className={isCelsius ? 'active' : ''}>°C</button>
-                    <button onClick={() => setIsCelsius(false)} className={!isCelsius ? 'active' : ''}>°F</button>
-                </div>
-            )}
-
-            {weather && (
-                <Forecast forecastData={forecastData} isCelsius={isCelsius} /> // Verwende die Forecast-Komponente
-            )}
-
-            {weather && (
-                <Highlights highlights={highlights} /> // Verwende die Highlights-Komponente
-            )}
+      {weather && (
+        <>
+            <Forecast forecastData={forecastData} isCelsius={isCelsius} />
+            <Highlights highlights={highlights} /> 
+        <div className="header">
+            {city}, {getCountryFlagEmoji(country)} UTC{timezoneOffsetFormatted}
         </div>
-    );
+        <div className="toggle-buttons">
+          <button
+            onClick={() => setIsCelsius(true)}
+            className={isCelsius ? "active" : ""}
+          >
+            °C
+          </button>
+          <button
+            onClick={() => setIsCelsius(false)}
+            className={!isCelsius ? "active" : ""}
+          >
+            °F
+          </button>
+        </div>
+        </>
+      )}
+      {error && <p className="errorDisplay">{error}</p>}  
+    </div>
+  );
 }
--- src/components/UVIndex.js
+++ src/components/UVIndex.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import './UVIndex.css';
-import { getStatusUV } from '../utils/statusUtils';
+import React from "react";
+import "./UVIndex.css";
+import { getStatusUV } from "../utils/statusUtils";
 
 const UVIndex = ({ value, status }) => {
-    const uvLevel = getStatusUV(value).toLowerCase().split(' ')[0]; // Extrahiere den UV-Level
-    console.log(uvLevel);
-    
-    return (
-        <div className={`uv-index ${uvLevel}`}>
-            <div className="uv-bar" style={{ width: `${(value / 11) * 100}%` }}></div>
-            <div className="uv-value">{value}</div>
-            <div className="uv-status">{getStatusUV(value)}</div>
-        </div>
-    );
+  const uvLevel = getStatusUV(value).toLowerCase().split(" ")[0]; // Extrahiere den UV-Level
+  console.log(value);
+  console.log(uvLevel);
+
+  const maxAngle = 180; // Halbkreis geht bis 180 Grad
+  const angle = (value / 17) * maxAngle; // UV-Index bestimmt Füllwinkel
+
+  return (
+    <div className="uv-container">
+        <div className="half-mask-container">
+            <div
+                className="circle1"
+                style={{ 
+                    WebkitMaskImage: `conic-gradient(from -90deg,rgb(245, 55, 255) 0deg, rgb(242, 0, 255) ${angle}deg, rgba(251, 0, 255, 0) ${angle}deg, rgb(255, 0, 234) 180deg)`,
+                    maskImage: `conic-gradient(from -90deg,rgb(75, 55, 255) 0deg, rgb(0, 30, 255) ${angle}deg, rgba(0, 38, 255, 0) ${angle}deg, rgb(0, 64, 255) 180deg)`
+                }}
+            >
+                <div className="circle2">
+                </div>
+            </div>
+        <div className="uv-value">{value}</div>
+                
+    </div>
+    </div>
+  );
 };
 
 export default UVIndex;
